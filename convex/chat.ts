0c3a6ad0665f10cfcfdec609edcfa5d4113cca03
ankit-pfc <ankitmishra@projectfitco.com>
2026-02-05 12:46:42 +0530

    feat: Implement a Wikipedia summary bot and fetch chat messages from the Convex backend.

--- convex/chat.ts
+++ convex/chat.ts
@@ -1,16 +1,57 @@
-import { mutation } from "./_generated/server";
+import { query, mutation, internalAction, QueryCtx, MutationCtx } from "./_generated/server";
+import { api, internal } from "./_generated/api";
+
 import { v } from "convex/values";
+
+export const getMessages = query({
+    args: {},
+    handler: async (ctx: QueryCtx) => {
+        // Get most recent messages first
+        const messages = await ctx.db.query("messages").order("desc").take(50);
+        // Reverse the list so that it's in a chronological order.
+        return messages.reverse();
+    },
+});
 
 export const sendMessage = mutation({
     args: {
         user: v.string(),
         body: v.string(),
     },
-    handler: async (ctx, args) => {
+    handler: async (ctx: MutationCtx, args) => {
         console.log("This TypeScript function is running on the server.");
         await ctx.db.insert("messages", {
             user: args.user,
             body: args.body,
         });
+
+        if (args.body.startsWith("/wiki")) {
+            // Get the string after the first space
+            const topic = args.body.slice(args.body.indexOf(" ") + 1);
+            await ctx.scheduler.runAfter(0, internal.chat.getWikipediaSummary, {
+                topic,
+            });
+        }
     },
 });
+
+export const getWikipediaSummary = internalAction({
+    args: { topic: v.string() },
+    handler: async (ctx, args) => {
+        const response = await fetch(
+            "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=" +
+            args.topic,
+        );
+
+        const summary = getSummaryFromJSON(await response.json());
+        await ctx.scheduler.runAfter(0, api.chat.sendMessage, {
+            user: "Wikipedia",
+            body: summary,
+        });
+    },
+});
+
+function getSummaryFromJSON(data: any) {
+    const firstPageId = Object.keys(data.query.pages)[0];
+    return data.query.pages[firstPageId].extract;
+}
